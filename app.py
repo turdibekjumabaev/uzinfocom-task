03603e5392a161b76f3a3e7cd4c19af48e645925
Turdıbek Jumabaev <turdibekjumabaev05@gmail.com>
2024-10-21 14:19:09 +0500

    Update app.py for improved functionality

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 
 from src.loader import init_app
+from src.services.worker import process_sms
+
+import threading
 
 app = Flask(__name__)
+jwt = JWTManager(app)
 init_app(app)
 
 
+def start_worker():
+    worker = threading.Thread(target=lambda: process_sms(app))
+    worker.start()
+
+
 if __name__ == "__main__":
+    start_worker()
     app.run(port=8080)
