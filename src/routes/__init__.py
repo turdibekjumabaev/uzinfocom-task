1d2d659a534f0b781de3cd585dcb5f0760503002
Turdıbek Jumabaev <turdibekjumabaev05@gmail.com>
2024-10-21 14:21:03 +0500

    Refine initialization logic in src/routes/__init__.py

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask, Blueprint
 
+from .otp import otp_bp
+from .auth import auth_bp
+
 main_bp = Blueprint('main', __name__)
 
 
@@ -10,3 +13,5 @@ def index():
 
 def init_routes(app: Flask):
     app.register_blueprint(main_bp)
+    app.register_blueprint(otp_bp, url_prefix='/api/otp')
+    app.register_blueprint(auth_bp, url_prefix='/api/auth')
