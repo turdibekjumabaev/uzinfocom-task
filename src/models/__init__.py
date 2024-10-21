f293abdfc53a31c749ffc29d20057bb2e758d112
Turdıbek Jumabaev <turdibekjumabaev05@gmail.com>
2024-10-21 14:20:27 +0500

    Refine initialization logic in src/models/__init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,10 +1,15 @@
 from src.dp import db
+from src.dataloader import load_data
 
 
 def init_db(app):
     db.init_app(app)
     with app.app_context():
         db.create_all()
+        load_data(db)
 
 
 # Models
+from .otp import OTP
+from .role import Role
+from .user import User
