37c5c78e223f88af44dfdf52e0432a36c7101227
Turdıbek Jumabaev <turdibekjumabaev05@gmail.com>
2024-10-21 20:45:08 +0500

    code optimized

--- src/services/eskiz.py
+++ src/services/eskiz.py
@@ -1,4 +1,8 @@
 import requests
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class Eskiz:
@@ -29,6 +33,8 @@ class Eskiz:
         }
 
         response = requests.post(f'{self.base_url}/api/auth/refresh', headers=headers)
+        if response.status_code == 401:
+            logger.info(f'Eskiz login or password is incorrect')
         return response.json()
 
     def send_sms(self, mobile_phone, message):
@@ -44,4 +50,12 @@ class Eskiz:
             'Authorization': f'Bearer {self.token}'
         }
         response = requests.post(f'{self.base_url}/api/message/sms/send', data=payload, headers=headers)
+
+        if response.status_code == 200:
+            logger.info(f'Eskiz sms sent to {mobile_phone}')
+        elif response.status_code == 401:
+            logger.error(f'Eskiz authentication failed')
+        elif response.status_code == 400:
+            logger.error(f'Failed to send sms to {mobile_phone}: {response.json().get("message")}')
+
         return response
